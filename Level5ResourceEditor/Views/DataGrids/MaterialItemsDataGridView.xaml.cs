4b83bbe77da70c32abf1e99c449b649ed8d95474
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2025-11-29 07:23:51 +0100

    Separates the style of the datagrids and the MainContent
    Now the datagrid style is counted as a view, and therefore the main content calls this view.

---
+++ Level5ResourceEditor/Views/DataGrids/MaterialItemsDataGridView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Level5ResourceEditor.Views.DataGrids
+{
+    /// <summary>
+    /// Logique d'interaction pour MaterialItemsDataGridView.xaml
+    /// </summary>
+    public partial class MaterialItemsDataGridView : UserControl
+    {
+        public MaterialItemsDataGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
